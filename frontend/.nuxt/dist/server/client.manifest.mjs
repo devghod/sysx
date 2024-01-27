0767fc0fc6f5bc8bc5bf426b9802095866b0872d
Ghodie <ghodie@nathandigital.com>
2024-01-27 18:55:49 +0800

    Ignore .nuxt, package-lock.json, and node_modules

--- frontend/.nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/ghodiemadrona/Desktop/Dev/sysx/web/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- frontend/.nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/ghodiemadrona/Desktop/Dev/sysx/web/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
